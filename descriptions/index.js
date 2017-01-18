af0abfdbf7e556ef2886056cd05c7a3cbf60fb8c
Jeremy Likness <jeremy@jeremylikness.com>
2017-01-18 16:11:18 -0500

    Add descriptions service and update compose

---
+++ descriptions/index.js
@@ -0,0 +1,17 @@
+const {send} = require('micro'),
+    mongojs = require("mongojs");
+
+module.exports = async (req, res) => {
+    try {
+        let db = mongojs('db/usda', ['fooddescriptions']),
+        results = db.fooddescriptions.find({}).toArray(function (err, docs) {
+            if (err) {
+                throw err;
+            }
+            send(res, 200, docs);
+        });
+    }
+    catch(e) {
+        send(res, 500, e.message);
+    }
+};
