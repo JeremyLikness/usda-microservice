3a206f22ef06153be69f0ed1559f6884b8bebc8b
Jeremy Likness <jeremy@jeremylikness.com>
2017-01-19 10:20:34 -0500

    Initial commit of web project

---
+++ usdaweb/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { UsdawebPage } from './app.po';
+
+describe('usdaweb App', function() {
+  let page: UsdawebPage;
+
+  beforeEach(() => {
+    page = new UsdawebPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
