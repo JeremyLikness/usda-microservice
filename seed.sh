2928a66e1687bd02dcdaa9580e07ba9ee1b47eb9
Jeremy Likness <jeremy@jeremylikness.com>
2017-02-08 10:18:23 -0500

    Update seed to process quotes and import extras

--- seed.sh
+++ seed.sh
@@ -14,17 +14,27 @@ while [[ RET -ne 0 ]]; do
 done
 
 files=("FOOD_DES.txt" \
- "FD_GROUP.txt")
+ "FD_GROUP.txt" \
+ "NUT_DATA.txt" \
+ "NUTR_DEF.txt" \
+ "WEIGHT.txt")
 collections=("fooddescriptions" \
- "foodgroups")
+ "foodgroups" \
+ "nutrientdata" \
+ "nutrientdefinitions" \
+ "weight")
 fields=("id,group-id,description,name,alias,manufacturer,survey,refuse,refuse-percentage,scientific-name,nitrogen-factor,protein-factor,fat-factor,carbohydrate-factor" \
-    "id,name")
+    "id,name" \
+    "food-id,nutrient-id,hundred-grams,data-points,std-error,code,derivation-code,ref-id,nutrient-mark,studies,min,max,degrees-freedom,low-error,high-error,comments,add-date,confidence" \
+    "id,uom,tag,description,round,order" \
+    "food-id,sequence,amount,description,gram-weight,data-points,deviation")
 
 idx=0 
 
 for file in "${files[@]}" 
 do 
 
+sed -i 's/\"/''/g' /seed/$file
 sed -i 's/\~/\"/g' /seed/$file
 sed -i 's/\^/,/g' /seed/$file
 collection=${collections[idx]}
